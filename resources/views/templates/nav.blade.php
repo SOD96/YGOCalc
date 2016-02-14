c718da0ac3974abd682fe687b4ed64af166b9ff2
Sean O'Donnell <xengaming@outlook.com>
2016-02-14 02:26:42 +0000

    fix nav dropdown

--- resources/views/templates/nav.blade.php
+++ resources/views/templates/nav.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-navbar" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -13,7 +13,7 @@
                 </a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse">
+            <div class="collapse navbar-collapse" id="bs-navbar">
                 <ul class="nav navbar-nav">
                     <li>
                         <a href="{{URL::to('create')}}">Create Duel</a>
